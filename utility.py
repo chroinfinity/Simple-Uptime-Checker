93607336727ea8da8baabb625594370f79edbe2d
Hector <a19310176@live.ceti.mx>
2025-06-24 15:02:13 -0600

    First ping test

--- utility.py
+++ utility.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import subprocess
+
+timeout = 2
+host = "www.google.com"
+
+result = subprocess.run(
+            ['ping', '-c', '1', '-W', str(timeout), host],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+
+print(result)
